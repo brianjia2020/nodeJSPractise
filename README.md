070b32f09570dbdb24cce3e58bb3f52d43652e52
brianjia2020 <brianjia1123@gmail.com>
2021-05-01 20:16:02 -0400

    commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 My practise on Node.JS
+
+### npm: node package manager
+
+npm commands: - npm -v: check npm version - npm version: check all version info - npm search package-name : search package name in the platform - npm install package-name : install the required packages - npm remove package-name : remove the package from project
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var a = 4
 var fs = require("fs")
+var math = require("Math")
 
 console.log(global.a)
 
@@ -9,7 +10,10 @@ console.log(global.a)
  *    -node.js will wrap up this portion inside an function to perform
  *    -function (exports, require, module, __filename, __dirname){}
 */
-console.log(arguments.callee + "")
+// console.log(arguments.callee + "")
+
+
+console.log(math.add(123,456))
 
 
 
