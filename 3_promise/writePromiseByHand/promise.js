5415a5d3eafd357add4ce0a95ff6a08233c4cd81
brianjia2020 <brianjia1123@gmail.com>
2021-05-16 19:58:46 -0400

    added more

--- 3_promise/writePromiseByHand/promise.js
+++ 3_promise/writePromiseByHand/promise.js
@@ -1,19 +1,62 @@
 //self defined promise
 function Promise(executor){
     //resolve function
-    function resolve(data){
 
+    this.PromiseState = 'pending';
+    this.PromiseResult = null;
+    this.callbacks = [];
+    const self = this;
+
+    function resolve(data){
+        // console.log(this);
+        //1. modify object state(promiseState)
+        if(self.PromiseState !== 'pending') return;
+        self.PromiseState = 'fulfilled';
+        //2. modify object result(promiseResult)
+        self.PromiseResult = data;
+        //3. invoke the callback function
+        self.callbacks.forEach(item => {
+            item.onResolved(data);
+        });
     }
     //reject function
     function reject(data){
-
+        //check if state has been changed.
+        if(self.PromiseState !== 'pending') return;
+        self.PromiseState = 'rejected';
+        self.PromiseResult = data;
+        //3. invoke the callback function
+        self.callbacks.forEach(item => {
+            item.onRejected(data);
+        })
     }
-    //async invoke executor function
-    executor(resolve,reject);
+
+    try{
+        //async invoke executor function
+        executor(resolve,reject);
+    } catch(e){
+        //modify the promise state as reject when handling an error
+        reject(e);
+    }   
 }
 
 //add then function
-
 Promise.prototype.then = function(onResolved, onRejected){
-
+    //call up callback function PromiseState
+    if (this.PromiseState === 'fulfilled'){
+        onResolved(this.PromiseResult);
+    }
+    if (this.PromiseState === 'rejected'){
+        onRejected(this.PromiseResult);
+    }
+    //check pending state
+    if (this.PromiseState === 'pending'){
+        //save the callback function
+        //better to use an array or list to store all the callbacks
+        
+        this.callbacks.push({
+            onResolved : onResolved,
+            onRejected : onRejected
+        });
+    }
 }
