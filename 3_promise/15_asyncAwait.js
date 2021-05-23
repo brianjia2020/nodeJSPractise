6344173b975eeea6795a394c3b2f9f8b131fa7d5
brianjia2020 <brianjia1123@gmail.com>
2021-05-22 20:32:59 -0400

    new stuff

---
+++ 3_promise/15_asyncAwait.js
@@ -0,0 +1,34 @@
+/**
+ * read files under resource folder
+ */
+
+const fs = require("fs")
+const util = require("util")
+const mineReadFile = util.promisify(fs.readFile)
+
+//use call backs
+// fs.readFile("./resource/1.txt", (err,data1) =>{
+//     if (err) throw err;
+//     fs.readFile("./resource/2.txt", (err,data2) =>{
+//         if (err) throw err;
+//         fs.readFile("./resource/3.txt", (err,data3) =>{
+//             if (err) throw err;
+//             console.log(data1+data2+data3)
+//         })
+//     })
+// })
+
+//use async and await
+async function main(){
+    try{
+        let data1 = await mineReadFile('./resource/1.txt')
+        let data2 = await mineReadFile('./resource/2.txt')
+        let data3 = await mineReadFile('./resource/3.txt')
+
+        console.log(data1+data2+data3)
+    } catch(e){
+        console.log(e)
+    }
+}
+
+main()
